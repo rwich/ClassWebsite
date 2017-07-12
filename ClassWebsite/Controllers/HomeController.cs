f18e816dc6f896c10126f95db4811815d5b7b844
rwich <rwich@users.noreply.github.com>
2017-07-11 19:39:15 -0700

    Add project files.

---
+++ ClassWebsite/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ClassWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassWebsite.MvcApplication" Language="C#" %>
