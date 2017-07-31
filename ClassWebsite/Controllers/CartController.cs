1ac4d345da5be7fff22b50e2dfe62d4646119b07
rwich <rwich@users.noreply.github.com>
2017-07-31 15:00:03 -0700

    Added basic shopping cart functionality

---
+++ ClassWebsite/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using ClassWebsite.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddToCart(int id)
+        {
+            Product p = ProductDB.GetProductById(id);
+            CartItem item = new CartItem();
+            item.PriceWhenAdded = p.RetailPrice;
+            item.Quantity = 1;
+            item.ProductID = p.ProductID;
+
+            HttpCookie product = new HttpCookie("Cart");
+            product.Value = JsonConvert.SerializeObject(item);
+            product.Expires = DateTime.Now.AddYears(10);
+            Response.Cookies.Add(product);
+
+            return RedirectToAction("ViewCart", "Cart");
+        }
+
+        public ActionResult ViewCart()
+        {
+            //get CartItems from cookie
+            CartItem item = JsonConvert.DeserializeObject<CartItem>(Request.Cookies["Cart"].Value);
+            
+            return View(item);
+        }
+    }
+}
---
+++ ClassWebsite/Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassWebsite.Models
+{
+    /// <summary>
+    /// Represents a single product in the end-user's cart
+    /// </summary>
+    public class CartItem
+    {
+        public int ProductID { get; set; }
+
+        public short Quantity { get; set; }
+
+        public double PriceWhenAdded { get; set; }
+
+
+    }
+}
